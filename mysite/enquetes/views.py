78b3bbc6f7da0c05f3a87b602af662dbfd210e9e
leo <leo.profissional10@gmail.com>
2024-08-07 19:26:09 -0300

    ajeitando projeto

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,17 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from .models import Pergunta, Escolha
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse("Ola mundo, Voce esta no index de enquetes. ")
+    ultimas_perguntas = Pergunta.objects.order_by('-data_publicacao')[:5]
+    contexto = { 'ultimas_perguntas': ultimas_perguntas }
+    return render(request,'enquetes/index.html',contexto)
 
-def detalhes(request, pergunta_id):
-    return HttpResponse(f"Voce esta vendo os resultado da questao{pergunta_id}")
-
-def resultado(request, pergunta_id):
-    return HttpResponse(f"Voces esta vendo os resultados da pergunta{pergunta_id}")
-
-def votos(request, pergunta_id):
-    pergunta = get_object_or_404(pergunta, pk=pergunta_id)
+def detalhes(request,pergunta_id):
     try:
-        escolha_selecionada = pergunta.escolha_set.get(pk=request.POST("escolha"))
-        
+        pergunta = Pergunta.objects.get(pk=pergunta_id)
+    except:
+        raise Http404("A pergunta não existe!")
+    return render(request, 'enquetes/detalhes.html',{'pergunta': pergunta})
+
+def resultados(request,pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return render(request, "enquetes/resultados.html", {"pergunta": pergunta})
+
+def votos(request,pergunta_id):
+        pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+        try:
+            escolha_selecionada = pergunta.escolha_set.get(pk=request.POST["escolha"])
+        except (KeyError, Escolha.DoesNotExist):
+            # Redisplay the question voting form.
+            return render(  
+                request,
+                "enquetes/detalhes.html",
+                {
+                    "question": pergunta,
+                    "error_message": "Você não selecionou uma escolha.",
+                },
+            )
+        else:
+            escolha_selecionada.votos = F("votos") + 1
+            escolha_selecionada.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse("enquetes:resultados", args=(pergunta.id,)))
